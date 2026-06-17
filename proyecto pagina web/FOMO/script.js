41b5190c33f74adbee165a24e8da9d38d83dbda1
josefinaalonso016-beep <josefinaalonso016@gmail.com>
2026-06-17 08:25:12 -0300

    Add files via upload

--- proyecto pagina web/FOMO/script.js
+++ proyecto pagina web/FOMO/script.js
@@ -1,2 +1,47 @@
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+function scrollGallery(amount) {
+
+  const gallery = document.getElementById("galleryTrack");
+
+  gallery.scrollBy({
+    left: amount,
+    behavior: "smooth"
+  });
+}
+const openModal = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+const modal = document.getElementById("modal");
+
+openModal.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+const showRegister = document.getElementById("show-register");
+
+showRegister.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  document.querySelector(".modal-content h2").textContent = "Registrarse";
+
+  document.querySelector(".login-btn").textContent = "Crear cuenta";
+
+  document.querySelector(".register-text").innerHTML =
+    '¿Ya tenés cuenta? <a href="#" id="show-login">Iniciar sesión</a>';
+});
+
 
 
